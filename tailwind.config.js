5b3e2d72d23e16bc6a742d7fbc1590e61ba8ae4a
Mariosimom <simoncik.mario@gmail.com>
2025-08-16 18:17:49 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        "brand-gold": "#FFD700" // gold color
+      }
+    }
+  },
   plugins: [],
 };
